6efc475769e4dc31b75442aed328bc19054b791c
Martin Koller <kollix@aon.at>
2017-11-05 19:13:51 +0100

    set AA_UseHighDpiPixmaps

--- desktop.cxx
+++ desktop.cxx
@@ -36,6 +36,8 @@ int main(int argc, char **argv)
   QCoreApplication::setAttribute(Qt::AA_UseStyleSheetPropagationInWidgetStyles);
 #endif
 
+  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
   QApplication app(argc, argv);
 
   KLocalizedString::setApplicationDomain("liquidshell");
