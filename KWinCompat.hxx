bb021b4c24ff912a46ec3a05c3c76feeb2bc8693
Martin Koller <kollix@aon.at>
2023-04-13 14:47:21 +0200

    add new include file

---
+++ KWinCompat.hxx
@@ -0,0 +1,16 @@
+#ifndef _KWINCOMPAT_H_
+#define _KWINCOMPAT_H_
+
+// make porting to KF5 >= 5.101 simpler
+
+#include <kwindowsystem_version.h>
+#include <KWindowSystem>
+
+#if KWINDOWSYSTEM_VERSION >= QT_VERSION_CHECK(5, 101, 0)
+#  include <kx11extras.h>
+#  define KWinCompat KX11Extras
+#else
+#  define KWinCompat KWindowSystem
+#endif
+
+#endif
