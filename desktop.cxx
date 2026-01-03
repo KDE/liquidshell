d62d7138acd356b22e86556c2d9e0464ada9b578
Martin Koller <martin@kollix.at>
2026-01-03 12:53:18 +0100

    Bump version

--- desktop.cxx
+++ desktop.cxx
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 /*
-  Copyright 2017 - 2023 Martin Koller, martin@kollix.at
+  Copyright 2017 - 2026 Martin Koller, martin@kollix.at
 
   This file is part of liquidshell.
 
@@ -43,10 +43,10 @@ int main(int argc, char **argv)
 
   KLocalizedString::setApplicationDomain("liquidshell");
 
-  KAboutData aboutData("liquidshell", i18n("Liquidshell"), "1.9",
+  KAboutData aboutData("liquidshell", i18n("Liquidshell"), "1.9.1",
                        i18n("A QtWidgets based basic desktop shell"),
                        KAboutLicense::GPL_V3,
-                       i18n("Copyright 2017 - 2023 Martin Koller"), QString(),
+                       i18n("Copyright 2017 - 2026 Martin Koller"), QString(),
                        "https://apps.kde.org/de/liquidshell"); // homepage
                        //"https://www.linux-apps.com/p/1205621"); // homepage
 
